2e0723c2600157a1e1e4b20cf23e0da246552737
Ben Hutchison <ben@aldaviva.com>
2014-01-07 22:37:06 -0800

    When changing tabs, pause tab cycling until the user hasn't changed tabs for 20 seconds.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var PowerMate	= require('node-powermate');
-var http	= require('http');
 var _		= require('lodash');
 var config	= require('./config');
+var request     = require('request');
 
 var BROWSER_ID = config.browserId;
 var PANOPTICHROME_API_ROOT = config.apiRoot;
+var DELAY_BEFORE_RESUMING_TAB_CYCLE = 20*1000;
 
 var powermate;
+var resumeTabCycleDebounced;
 
 try {
     powermate = new PowerMate();
 } catch(err){
-    if(err.indexOf("cannot open device with path") === 0 && process.getuid() !== 0){
+    if(err.indexOf && err.indexOf("cannot open device with path") === 0 && process.getuid() !== 0){
 	console.error("Unable to open HID device. Make sure you are running panoptichrome-powermate as root.");
     } else {
-	console.error(err);
+	console.error(err.message);
     }
     process.exit(1);
 }
@@ -23,17 +25,15 @@ var wheelClicks = 0;
 powermate.on('wheelTurn', _.wrap(function(wheelDelta){
     var isClockwise = (wheelDelta > 0);
     var direction = (isClockwise ? 'next' : 'previous');
-    
-    var req = http.request({
-	method: 'PATCH',
-	host: PANOPTICHROME_API_ROOT.host,
-	port: PANOPTICHROME_API_ROOT.port,
-	path: '/cgi-bin/browsers/'+BROWSER_ID+'/tabs/'+direction,
-	headers: {
-	    'Content-Type': 'application/json'
-	}
+  
+    request({
+	url: PANOPTICHROME_API_ROOT+"/browsers/"+BROWSER_ID+"/tabs/"+direction,
+	method: "PATCH",
+	json: { active: true }
     });
-    req.end(JSON.stringify({ active: true }));
+
+    pauseTabCycle();
+    
     console.log(direction+" tab");
 }, function(inner, wheelData){
     if((++wheelClicks % 8) === 0){
@@ -43,18 +43,55 @@ powermate.on('wheelTurn', _.wrap(function(wheelDelta){
 }));
 
 powermate.on('buttonDown', function(){
-    var req = http.request({
-	method: 'POST',
-	host: PANOPTICHROME_API_ROOT.host,
-	port: PANOPTICHROME_API_ROOT.port,
-	path: '/cgi-bin/browsers/'+BROWSER_ID+'/message',
-	headers: {
-	    'Content-Type': 'application/json'
+    request({
+	url: PANOPTICHROME_API_ROOT+"/browsers/"+BROWSER_ID+"/message",
+	method: "POST",
+	json: {
+	    powermate: { buttonPressed: true }
 	}
     });
-    req.end(JSON.stringify({ powermate: { buttonPressed: true }}));
     console.log("button pressed");
 });
+
+function pauseTabCycle(){
+    request({
+	url: PANOPTICHROME_API_ROOT+"/browsers/"+BROWSER_ID,
+	json: true
+    }, function(err, res, body){
+	var wasPaused = body.isCyclePaused;
+	
+	if(!wasPaused){
+	    console.log("pausing tab cycle");
+	    request({
+		url: PANOPTICHROME_API_ROOT+"/browsers/"+BROWSER_ID,
+		method: 'PATCH',
+		json: {
+		    isCyclePaused: true
+		}
+	    });
+
+	    resumeTabCycleDebounced = _.debounce(resumeTabCycle, DELAY_BEFORE_RESUMING_TAB_CYCLE);
+	    resumeTabCycleDebounced();
+	} else {
+	    console.log("tab cycle already paused, ignoring");
+	}
+    });
+
+    resumeTabCycleDebounced && resumeTabCycleDebounced();
+}
+
+function resumeTabCycle(){
+    resumeTabCycleDebounced = null;
+
+    console.log("resuming tab cycle");
+    request({
+	url: PANOPTICHROME_API_ROOT+"/browsers/"+BROWSER_ID,
+	method: 'PATCH',
+	json: {
+	    isCyclePaused: false
+	}
+    });
+}
 
 powermate.on('buttonUp', function(){
     //console.log('button released');
